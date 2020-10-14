e173baa6a800bacfecb580e8d2a4d97a9fdf4093
Sergiusz Kriwobokow <writesd@hotmail.com>
2020-10-14 14:51:28 +0200

    no comments

---
+++ FieldServiceT/Models/BookableResourceBooking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FieldServiceT.Models
+{
+    public class BookableResourceBooking
+    {
+        public string BookableBesourceBookingId { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public int Duration { get; set; }
+        public DateTime EndTime { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        public int StateCode { get; set; }
+    }
+}
