547e4693b5e4bfc63dba1d17c67d9f8395d46e22
Sergiusz Kriwobokow <writesd@hotmail.com>
2020-10-16 04:10:29 +0200

    no comments

--- FieldServiceT/Models/BookableResourceBooking.cs
+++ FieldServiceT/Models/BookableResourceBooking.cs
@@ -10,12 +10,27 @@ namespace FieldServiceT.Models
     {
         [Key]
         public string BookableResourceBookingId { get; set; }
-        [Display(Name="CREATED ON")]
+        [Display(Name = "CREATED ON")]
         public DateTime CreatedOn { get; set; }
         public int Duration { get; set; }
         public DateTime EndTime { get; set; }
         public DateTime ModifiedOn { get; set; }
         public string StatusCode { get; set; }
+        public string Name { get; set; }
+
+        private BookingStatus _bookingStatus;
+        public BookingStatus BookingStatus
+        {
+            set
+            {
+                StatusName = value.Name;
+            }
+        }
+
+        public string StatusName { get; set; }
+    }
+    public class BookingStatus
+    {
         public string Name { get; set; }
     }
 }
