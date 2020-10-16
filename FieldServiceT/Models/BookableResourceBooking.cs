2d10515a08c70f8c167e53c0a8707c0df8e2cfaf
Sergiusz Kriwobokow <writesd@hotmail.com>
2020-10-16 11:21:45 +0200

    added BookedResourcePage

--- FieldServiceT/Models/BookableResourceBooking.cs
+++ FieldServiceT/Models/BookableResourceBooking.cs
@@ -17,8 +17,6 @@ namespace FieldServiceT.Models
         public DateTime ModifiedOn { get; set; }
         public string StatusCode { get; set; }
         public string Name { get; set; }
-
-        private BookingStatus _bookingStatus;
         public BookingStatus BookingStatus
         {
             set
