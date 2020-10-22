58e646cfa56a30090c4cc476ec0a9a7ea6b4aff9
Sergiusz Kriwobokow <writesd@hotmail.com>
2020-10-22 22:06:29 +0200

    completed BoookableresourceBooking

--- FieldServiceT/Models/BookedResource.cs
+++ FieldServiceT/Models/BookedResource.cs
@@ -3,27 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
 
 namespace FieldServiceT.Models
 {
     public class BookedResource
     {
         [Key]
+        [JsonProperty("bookableresourcebookingid")]
         public string BookableResourceBookingId { get; set; }
-        public DateTime CreatedOn { get; set; }
-        public int Duration { get; set; }
-        public DateTime EndTime { get; set; }
-        public DateTime ModifiedOn { get; set; }
-        public string StatusCode { get; set; }
+        
+        [JsonProperty("name")]
         public string Name { get; set; }
-        public BookingStatus BookingStatus
-        {
-            set
-            {
-                StatusName = value.Name;
-            }
-        }
-        public string StatusName { get; set; }
-        //public msdyn_workorder WorkOrder { get; set; }
+
+        [JsonProperty("starttime")]
+        public DateTime StartTime { get; set; }
+
+        [JsonProperty("endtime")]
+        public DateTime EndTime { get; set; }
+
+        [JsonProperty("msdyn_workorder")]
+        public WorkOrder WorkOrder { get; set; }
     }
 }
---
+++ FieldServiceT/Models/WorkOrder.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FieldServiceT.Models
+{
+    public class WorkOrder
+    {
+        public WorkOrder() { }
+
+        [JsonProperty("msdyn_name")]
+        public string Name { get; set; }
+
+        [JsonProperty("msdyn_addressname")]
+        public string Addressname { get; set; }
+
+        [JsonProperty("msdyn_address1")]
+        public string Address1 { get; set; }
+    }
+}
+
+
+// msdyn_name
+// new_title – этого поля нет в тестовой среде
+// new_assignmentids - этого поля нет в тестовой среде
+// msdyn_addressname
+// msdyn_address1
