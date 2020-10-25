0812811e4dd8ee1a6f707112752f6490bb1b9af8
Sergiusz Kriwobokow <writesd@hotmail.com>
2020-10-25 23:52:53 +0100

    tiny refactoring

---
+++ FieldServiceT/Models/ODataResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FieldServiceT.Models
+{
+    internal class ODataResponse<T>
+    {
+        public List<T> Value
+        {
+            get;
+            set;
+        }
+    }
+}
