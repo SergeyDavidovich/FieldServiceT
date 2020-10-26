eb91c980070951f841127539fe32e3a297ca0a17
Sergiusz Kriwobokow <writesd@hotmail.com>
2020-10-26 20:43:00 +0100

    added sending web hook by BookedResourcePage (subject for refactoring to service)

---
+++ FieldServiceT/Models/Location.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace FieldServiceT.Models
+{
+    public class LocationValue
+    {
+        [JsonProperty("Latitude")]
+        public string Latitude { get; set; }
+
+        [JsonProperty("Longitude")]
+        public string Longitude { get; set; }
+
+        [JsonProperty("BKBid")]
+        public string BKBid { get; set; }
+
+        [JsonProperty("ButtonId")]
+        public string ButtonId { get; set; }
+    }
+}
